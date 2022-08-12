26a10c4f6e161b9048054d16e7144aabbfbdd9e1
Murat Altun <mraltun@gmail.com>
2022-08-12 19:18:24 +0300

    Wi

--- src/components/widgets/Widget.jsx
+++ src/components/widgets/Widget.jsx
@@ -1,11 +1,94 @@
 import React from "react";
+import KeyboardarrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
 import "./Widget.scss";
 
-const Widget = () => {
+const Widget = ({ type }) => {
+  let data;
+
+  // Temporary
+  const amount = 100;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonOutlinedIcon
+            className='icon'
+            style={{ color: "crimson", backgroundColor: "rgba(255,0,0,0.2)" }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+          <ShoppingCartOutlinedIcon
+            className='icon'
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgba(218,165,32,0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+          <MonetizationOnOutlinedIcon
+            className='icon'
+            style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2)" }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: (
+          <AccountBalanceWalletOutlinedIcon
+            className='icon'
+            style={{ color: "purple", backgroundColor: "rgba(128,0,128,0.2)" }}
+          />
+        ),
+      };
+      break;
+    default:
+      break;
+  }
+
   return (
     <div className='widget'>
-      <div className='left'>left</div>
-      <div className='right'>right</div>
+      <div className='left'>
+        <div className='title'>{data.title}</div>
+        <div className='counter'>
+          {data.isMoney && "$"} {amount}
+        </div>
+        <div className='link'>{data.link}</div>
+      </div>
+      <div className='right'>
+        <div className='percentage positive'>
+          <KeyboardarrowUpIcon />
+          {diff}%
+        </div>
+        {data.icon}
+      </div>
     </div>
   );
 };
