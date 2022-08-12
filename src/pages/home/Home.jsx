76b5cbbccfc5c0edee4cf80f6e0694dd37e95058
Murat Altun <mraltun@gmail.com>
2022-08-12 13:18:59 +0300

    Added boilerplate pages and sass

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <h1 className='title'>MrAltun</h1>
+    </div>
+  );
+};
+
+export default Home;
