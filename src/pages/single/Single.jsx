5f96455e6fcdcc614d235e39d91c8169276baefb
Murat Altun <mraltun@gmail.com>
2022-08-21 14:35:25 +0300

    Single.scss done

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -9,6 +9,20 @@ const Single = () => {
       <Sidebar />
       <div className='singleContainer'>
         <Navbar />
+        <div className='top'>
+          <div className='left'>
+            <h1 className='title'>Information</h1>
+            <div className='item'>
+              <img
+                src='https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
+                alt=''
+                className='itemImg'
+              />
+            </div>
+          </div>
+          <div className='right'></div>
+        </div>
+        <div className='bottom'></div>
       </div>
     </div>
   );
