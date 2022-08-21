e60f48660d1d22f4500c3394ef753a2ca6fcf7b0
Murat Altun <mraltun@gmail.com>
2022-08-21 18:33:55 +0300

    Added props for New in the App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 import New from "./pages/new/New";
 import Single from "./pages/single/Single";
+import { userInputs, productInputs } from "./formsource";
 
 const App = () => {
   return (
@@ -16,12 +17,18 @@ const App = () => {
           <Route path='users'>
             <Route index element={<List />} />
             <Route path=':userId' element={<Single />} />
-            <Route path='new' element={<New />} />
+            <Route
+              path='new'
+              element={<New inputs={userInputs} title='Add New User' />}
+            />
           </Route>
           <Route path='products'>
             <Route index element={<List />} />
             <Route path=':productId' element={<Single />} />
-            <Route path='new' element={<New />} />
+            <Route
+              path='new'
+              element={<New inputs={productInputs} title='Add New Product' />}
+            />
           </Route>
         </Route>
       </Routes>
