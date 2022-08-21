a68416ea7ca1cbc9b7debbbcf3b28358f6524c1b
Murat Altun <mraltun@gmail.com>
2022-08-21 14:14:40 +0300

    Datatable scss is done

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { DataGrid } from "@mui/x-data-grid";
-import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
+import { userColumns, userRows } from "../../datatablesource";
+import { DataGrid } from "@mui/x-data-grid";
 import "./Datatable.scss";
 
 const Datatable = () => {
