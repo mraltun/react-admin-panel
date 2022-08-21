ddbc2a8a0c1f21f9b45806724d5137d55ea141f1
Murat Altun <mraltun@gmail.com>
2022-08-21 16:46:34 +0300

    Single component is done. Fixed Chart component

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
 import "./Single.scss";
 
 const Single = () => {
@@ -11,6 +13,7 @@ const Single = () => {
         <Navbar />
         <div className='top'>
           <div className='left'>
+            <div className='editButton'>Edit</div>
             <h1 className='title'>Information</h1>
             <div className='item'>
               <img
@@ -18,11 +21,35 @@ const Single = () => {
                 alt=''
                 className='itemImg'
               />
+              <div className='details'>
+                <h1 className='itemTitle'>Jane Doe</h1>
+                <div className='detailItem'>
+                  <span className='itemKey'>Email:</span>
+                  <span className='itemValue'>jane@doe.com</span>
+                </div>
+                <div className='detailItem'>
+                  <span className='itemKey'>Phone:</span>
+                  <span className='itemValue'>+1234567890</span>
+                </div>
+                <div className='detailItem'>
+                  <span className='itemKey'>Address:</span>
+                  <span className='itemValue'>Wall St. New York</span>
+                </div>
+                <div className='detailItem'>
+                  <span className='itemKey'>Country:</span>
+                  <span className='itemValue'>USA</span>
+                </div>
+              </div>
             </div>
           </div>
-          <div className='right'></div>
+          <div className='right'>
+            <Chart aspect={3 / 1} title='User Spending (Last 6 Months)' />
+          </div>
         </div>
-        <div className='bottom'></div>
+        <div className='bottom'>
+          <h1 className='title'>Last Transactions</h1>
+          <List />
+        </div>
       </div>
     </div>
   );
