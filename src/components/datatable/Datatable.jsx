5194cae7f89be1a3fd057d4c6e1798b046612506
Murat Altun <mraltun@gmail.com>
2022-08-21 13:36:31 +0300

    Added List imports and Datatable component and scss

---
+++ src/components/datatable/Datatable.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import "./Datatable.scss";
+
+const Datatable = () => {
+  return <div className='datatable'>Datatable</div>;
+};
+
+export default Datatable;
