09c464a087e5c460be3099c8bbb5d99458e8d729
Murat Altun <mraltun@gmail.com>
2022-08-22 13:14:52 +0300

    Added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,36 @@ import Login from "./pages/login/Login";
 import New from "./pages/new/New";
 import Single from "./pages/single/Single";
 import { userInputs, productInputs } from "./formsource";
+import "./style/Dark.scss";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/'>
-          <Route index element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='users'>
-            <Route index element={<List />} />
-            <Route path=':userId' element={<Single />} />
-            <Route
-              path='new'
-              element={<New inputs={userInputs} title='Add New User' />}
-            />
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='users'>
+              <Route index element={<List />} />
+              <Route path=':userId' element={<Single />} />
+              <Route
+                path='new'
+                element={<New inputs={userInputs} title='Add New User' />}
+              />
+            </Route>
+            <Route path='products'>
+              <Route index element={<List />} />
+              <Route path=':productId' element={<Single />} />
+              <Route
+                path='new'
+                element={<New inputs={productInputs} title='Add New Product' />}
+              />
+            </Route>
           </Route>
-          <Route path='products'>
-            <Route index element={<List />} />
-            <Route path=':productId' element={<Single />} />
-            <Route
-              path='new'
-              element={<New inputs={productInputs} title='Add New Product' />}
-            />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
